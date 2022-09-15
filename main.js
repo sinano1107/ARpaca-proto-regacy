0cf9de6b30cf0dfce1b1dcf427c9efbd67f90ef9
sinano1107 <tftf161107@icloud.com>
2022-09-15 13:50:38 +0900

    [add] ロード・ダウンロード機能を追加

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter.js";
 
 const canvas = document.getElementById("canvas");
 let scene, camera, renderer, controls, mesh;
@@ -96,8 +98,75 @@ document.getElementById("ARbutton").onclick = () => {
   params.append('material', mesh.material.constructor.name);
   params.append('wireframe', mesh.material.wireframe);
   params.append('position.y', mesh.position.y);
-  // console.log(params.toString());
-  // window.location.href = "/ar_page/ar.html?" + params.toString();
   const url = 'https://192.168.3.4:3000/ar_page/ar.html?' + params.toString();
-  navigator.clipboard.writeText(url);
+  // window.location.href = "/ar_page/ar.html?" + params.toString();
+  window.open(url, '_blank');
+  // navigator.clipboard.writeText(url);
+  // alert("URLをコピーしました");
+}
+
+document.getElementById("download-button").onclick = () => {
+  // let geometry, material, mesh;
+  // geometry = new THREE.BoxGeometry();
+  // material = new THREE.MeshNormalMaterial();
+  // mesh = new THREE.Mesh(geometry, material);
+  // mesh.position.x = 2;
+  // scene.add(mesh);
+  console.log(mesh.position);
+  // const json = mesh.toJSON();
+  const json = scene.toJSON();
+
+  const fileName = "scene.json";
+  const link = document.createElement("a");
+  link.href = "data:text/plain," + encodeURIComponent(JSON.stringify(json));
+  link.download = fileName;
+  link.click();
+}
+
+document.getElementById("load-button").onclick = () => {
+  const loader = new THREE.ObjectLoader();
+  loader.load('test.json', (obj) => {
+    console.log(obj.position);
+    obj.position.x = 3;
+    scene.add(obj);
+  });
+}
+
+document.getElementById("gltf-button").onclick = () => {
+  const light = new THREE.DirectionalLight(0xffffff, 0.8);
+  light.position.x = 3;
+  light.position.z = 3;
+  const loader = new GLTFLoader();
+  const meshFromGltf = loader.load("box.glb", (gltf) => {
+    console.log(gltf);
+    const mesh = gltf.scene.children[0];
+    mesh.position.z = 2;
+    scene.add(mesh, light);
+
+    const fileName = "gltf.json";
+    const link = document.createElement("a");
+    link.href = "data:text/plain," + encodeURIComponent(JSON.stringify(mesh.toJSON()));
+    link.download = fileName;
+    link.click();
+  });
+}
+
+document.getElementById("gltf-download-button").onclick = () => {
+  const exporter = new GLTFExporter();
+  exporter.parse(
+    mesh,
+    (gltf) => {
+      const link = document.createElement('a');
+      link.style.display = 'none';
+      console.log(gltf);
+      const blob = new Blob([gltf], { type: 'application/octet-stream' });
+      link.href = URL.createObjectURL(blob);
+      link.download = "scene.glb";
+      link.click();
+    },
+    (err) => console.error(err),
+    {
+      binary: true,
+    }
+  )
 }
